ef78a45731e60b35061bcdb4c5902af92fcc92fc
James Dougan <jdplaysgames200@gmail.com>
2025-08-01 16:55:13 +0100

    Windows gitops test

--- windows/main.ps1
+++ windows/main.ps1
@@ -1,1 +1,5 @@
 # Main windows Git-Ops Script
+
+# Test
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("GitOps update successful!", "Jam GitOps", 'OK', 'Information')
